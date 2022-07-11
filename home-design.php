9c726359992c2971230dc84d8308d3ba3ee6988d
Visalinthan <visalinthan@gmail.com>
2022-07-11 17:26:11 +0200

    design 2

--- home-design.php
+++ home-design.php
@@ -25,9 +25,10 @@
             </ul>
         </div>
         <div class="col-xl-11 col-lg-12">
-            <div class="bloc">
+            <div class="bloc" style="position: relative;">
                 <div class="bloc-right-up">
-                    <div class="triangle">
+                    <div class="triangle"
+                        style="border-width: 0 350px 300px 0; border-color: transparent #fff transparent transparent;">
                         <div class="content">
                             <a href="simulation.php" class="btn-simuler">Testez votre éligibilité</a>
                         </div>
@@ -35,7 +36,8 @@
                 </div>
 
                 <div class="bloc-left-down">
-                    <div class="triangle">
+                    <div class="triangle"
+                        style="border-width: 800px 0 0 500px; border-color: transparent transparent transparent #fff;">
                         <div class="content part">
                             <h1>Réalisez des économies, gagnez en confort et agissez pour la planète.</h1>
                         </div>
@@ -48,261 +50,70 @@
 
             </div>
 
-            <div class="bloc">
-                <div class="bloc-left-up">
-                    <div class="triangle">
-                        <div class="content">
-                            <p class="part">
-                                La Nationale Du Logement respecte une charte de qualité et possède toutes les
-                                certifications
-                                et labels permettant d’être Reconnu Garant de l'Environnement (RGE).
-                            </p>
-                            <p class="pro">
-                                Confier vos travaux à des professionnels experts en rénovation.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="bloc-center">
-
-                    <div class="col-md-12 part">
+            <div class="bloc row ">
+                <div class="col-md-8 ">
+                    <div class="content">
                         <div class="line"></div>
-                        <h2>Nous choisir c'est...</h2>
-                        <h4>...confier ses travaux à des professionnels experts dans leur domaine !</h4>
-                    </div>
-
-                    <div class="col-md-12 pro">
-                        <h2>Nous faire confiance</h2>
-                    </div>
-
-                    <div class="row bloc_4">
-                        <div class="col-md-4">
-                            <div class="choice part">
-                                <img src="img/home/ep_service.png" alt="Assistance Complète avec la LNDL" />
-                                <p>Une assistance complète</p>
-                            </div>
-
-                            <div class="choice pro">
-                                <h5>Accompagnement de A à Z</h5>
-                            </div>
-
-                        </div>
-                        <div class="col-md-4">
-                            <div class="choice part">
-                                <img src="img/home/Protect.png" alt="Certifié RGE avec la LNDL" />
-                                <p>Des artisans partenaires certifiés RGE</p>
-                            </div>
-
-                            <div class="choice pro">
-                                <h5>Un savoir faire reconnu</h5>
-                            </div>
-
-                        </div>
-                        <div class="col-md-4">
-                            <div class="choice part">
-                                <img src="img/home/Guarantee.png" alt="Garantie décénnale avec la LNDL" />
-                                <p>Garantie décénnale</p>
-                            </div>
-                            <div class="choice pro">
-                                <h5>RGE</h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="bloc-right-down">
-                    <div class="triangle">
-                        <div class="content">
-                            <a href="simulation.php" class="btn-simuler">Testez votre éligibilité</a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="bloc">
-                <div class="bloc-right-up">
-                    <div class="triangle">
-                        <div class="content">
-                            <a href="simulation.php" class="btn-simuler">Testez votre éligibilité</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="bloc-center">
-                    <div class="row">
                         <div class="col-md-12 part">
-                            <div class="line"></div>
-                            <h2>Nos Expertises</h2>
-                            <p>
-                                Acteur de la rénovation énergétique en France, La Nationale du Logement vous accompagne
-                                dans
-                                vos
-                                projets
-                                de rénovation.
-                            </p>
+                            <h2>Nous choisir c'est...</h2>
+
+                            <h4>...confier ses travaux à des professionnels experts dans leur domaine !</h4>
 
                         </div>
                         <div class="col-md-12 pro">
-                            <h2>Nos Prestations</h2>
+                            <h2>Nous faire confiance</h2>
+
                         </div>
-                        <div class="row part">
-
-                            <div class="col-md-3">
-                                <div class="service" id="img-iti">
-                                    <h3>ITE / ITI</h3>
-                                </div>
-
-                            </div>
-                            <div class="col-md-3">
-                                <div class="service" id="img-pac">
-                                    <h3>PAC</h3>
-                                </div>
-
-                            </div>
-                            <div class="col-md-3">
-                                <div class="service" id="img-vmc">
-                                    <h3>VMC</h3>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="service" id="img-tce">
-                                    <h3>TCE</h3>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row pro">
-
+                        <div class="row bloc_4">
                             <div class="col-md-4">
-                                <div class="flip-card">
-                                    <div class="flip-card-inner">
-                                        <div class="flip-card-front">
-                                            <h3>Rénovation énérgétique</h3>
-                                        </div>
-                                        <div class="flip-card-back">
-                                            <ul>
-                                                <li>Calorifugeage</li>
-                                                <li>Les points singuliers</li>
-                                                <li>Rééquilibrage de réseau hydraulique</li>
-                                                <li>ITE</li>
-                                                <li>Pompe à chaleur</li>
-                                                <li>VMC</li>
-                                            </ul>
-                                        </div>
-                                    </div>
+                                <div class="choice part">
+                                    <img src="img/home/ep_service.png" alt="Assistance Complète avec la LNDL" />
+                                    <p>Une assistance complète</p>
                                 </div>
-                            </div>
 
+                                <div class="choice pro">
+                                    <h5>Accompagnement de A à Z</h5>
+                                </div>
+
+                            </div>
                             <div class="col-md-4">
-                                <div class="flip-card">
-                                    <div class="flip-card-inner">
-                                        <div class="flip-card-front">
-                                            <h3>Tout corp d’état</h3>
-                                        </div>
-                                        <div class="flip-card-back">
-                                            <ul>
-                                                <li>Ventilation</li>
-                                                <li>Ravalement</li>
-                                                <li>Couverture étanchéité</li>
-                                                <li>Peinture</li>
-                                                <li>Revetement de sol</li>
-                                                <li>Plomberie</li>
-                                                <li>Electricité</li>
-                                                <li>Maçonnerie Platrerie</li>
-                                                <li>Gros Oeuvre</li>
-                                                <li>Second Oeuvre</li>
-                                            </ul>
-                                        </div>
-                                    </div>
+                                <div class="choice part">
+                                    <img src="img/home/Protect.png" alt="Certifié RGE avec la LNDL" />
+                                    <p>Des artisans partenaires certifiés RGE</p>
                                 </div>
-                            </div>
 
+                                <div class="choice pro">
+                                    <h5>Un savoir faire reconnu</h5>
+                                </div>
+
+                            </div>
                             <div class="col-md-4">
-                                <div class="flip-card">
-                                    <div class="flip-card-inner">
-                                        <div class="flip-card-front">
-                                            <h3>Démolition</h3>
-                                        </div>
-                                        <div class="flip-card-back">
-                                            <ul>
-                                                <li>Désamiantage</li>
-                                                <li>Déplombage</li>
-                                                <li>Curage</li>
-                                                <li>Déconstruction</li>
-                                                <li>Démolition</li>
-                                                <li>Recyclage</li>
-                                            </ul>
-                                        </div>
-                                    </div>
+                                <div class="choice part">
+                                    <img src="img/home/Guarantee.png" alt="Garantie décénnale avec la LNDL" />
+                                    <p>Garantie décénnale</p>
+                                </div>
+                                <div class="choice pro">
+                                    <h5>RGE</h5>
                                 </div>
                             </div>
-
-                        </div>
-
-                    </div>
-                </div>
-                <div class="bloc-left-down">
-                    <div class="triangle">
-                        <div class="content">
-                            <p class="part">
-                                Nous vous conseillons et nous réalisons vos projets sur-mesure, en restant disponible
-                                tout
-                                au
-                                long
-                                de vos travaux de rénovation. Votre confort est notre priorité et c'est la raison pour
-                                laquelle
-                                nous
-                                améliorons constamment la qualité de nos services.
-                            </p>
                         </div>
                     </div>
                 </div>
-
-            </div>
-
-            <div class="bloc">
-                <div class="bloc-left-up">
-                    <div class="triangle">
-                        <div class="content">
-                            <p>
-                                Avec la hausse des prix de l'énergie, ne rien faire coûte souvent plus cher !
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="bloc-center">
-                    <div class="row">
-                        <div class="line"></div>
-                        <div class="col-md-12 ">
-                            <h2>Nos certifications</h2>
-
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-3 certif">
-                            <img src="img/home/lndl-certification-rge-qualibat.png"
-                                alt="LNDL certification RGE Qualibat" />
-                        </div>
-                        <div class="col-md-3 certif">
-                            <img src="img/home/lndl-certification-rge-qualipac.png"
-                                alt="LNDL certification RGE QualiPac" />
-                        </div>
-                        <div class="col-md-3 certif">
-                            <img src="img/home/lndl-certification-rge-chauffage.png"
-                                alt="LNDL certification RGE Chauffage" />
-                        </div>
-                        <div class="col-md-3 certif">
-                            <img src="img/home/lndl-certification-rge-ventilation-plus.png"
-                                alt="LNDL certification RGE Ventilation Plus" />
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="bloc-right-down">
-                    <div class="triangle">
-                        <div class="content">
-                            <a href="simulation.php" class="btn-simuler">Testez votre éligibilité</a>
+                <div class="col-md-4" style="position: relative; height: 100%;">
+                    <p class="part">
+                        La Nationale Du Logement respecte une charte de qualité et possède toutes les
+                        certifications et
+                        labels permettant d’être Reconnu Garant de l'Environnement (RGE).
+                    </p>
+                    <p class="pro">
+                        Confier vos travaux à des professionnels experts en rénovation.
+                    </p>
+                    <div class="bloc-right-down">
+                        <div class="triangle"
+                            style="border-width: 0 0 200px 650px; border-color: transparent transparent #fff transparent;">
+                            <div class="content">
+                                <a href="simulation.php" class="btn-simuler">Testez votre éligibilité</a>
+                            </div>
                         </div>
                     </div>
                 </div>
