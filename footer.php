1921ec3ab2ed0341c60de524c48e52a2387ec3a3
Visalinthan <visalinthan@gmail.com>
2022-07-18 15:19:07 +0200

    f commit

---
+++ footer.php
@@ -0,0 +1,124 @@
+        <!-- Footer -->
+        <footer id="footer" class="clearfix">
+            <div id="footer-top" class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="footer-schedule">
+                            <div class="heading-schedule">
+                                <h2 class="section-40px-barlow font-weight-500">Need Free Consultation ?</h2>
+                            </div>
+                            <div class="button-footer">
+                                <a href="contact.html" class="button readmore">Book Schedule</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="footer-widgets" class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="logo-footer">
+                            <a href="index.html" rel="home" class="main-logo">
+                                <img src="assets/images/logo/logo-dark.png" alt="images">
+                            </a>
+                        </div>
+                        <p class="text-widget">Improve efficiency, provide a better customer experience with modern
+                            technology services around the world. Our skilled staff, combined</p>
+                        <div class="widget-social">
+                            <ul>
+                                <li><a href="#" class="facebook-icon"></a></li>
+                                <li><a href="#" class="twitter-icon"></a></li>
+                                <li><a href="#" class="linked-icon"></a></li>
+                                <li><a href="#" class="instagram-icon"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="footer-widgets-nav-menu text-white">
+                            <div class="menu-1">
+                                <h3 class="widget-title-link-wrap">Official Info</h3>
+                                <div class="widget-links">
+                                    <ul class="link-wrap">
+                                        <li><a href="about.html">About us</a></li>
+                                        <li><a href="service.html">Services</a></li>
+                                        <li><a href="portfolio.html">Portfolio</a></li>
+                                        <li><a href="team.html">Our Team</a></li>
+                                        <li><a href="contact.html">Contact us</a></li>
+                                    </ul>
+                                    <ul class="link-wrap">
+                                        <li><a href="service.html">Help</a></li>
+                                        <li><a href="about.html">History</a></li>
+                                        <li><a href="contact.html">Support</a></li>
+                                        <li><a href="contact.html">Privacy</a></li>
+                                        <li><a href="faq.html">Faq</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="menu-2">
+                                <h3 class="widget-title-link-wrap">Useful Links</h3>
+                                <ul class="link-wrap menu2">
+                                    <li><a href="service-details.html">Strategy & Planning</a></li>
+                                    <li><a href="service-details.html">Vision & Values</a></li>
+                                    <li><a href="service-details.html">Audit & Assurance</a></li>
+                                    <li><a href="service-details.html">Support & Maintenance</a></li>
+                                    <li><a href="service-details.html">Company History</a></li>
+                                </ul>
+                            </div>
+                            <div class="menu-3">
+                                <h3 class="widget-title-link-wrap style-2">Get in Touch</h3>
+                                <ul class="widget-list-contact link-style-4">
+                                    <li>
+                                        <span class="meta-address">66 Broklyant, India 3269 Road.</span>
+                                    </li>
+                                    <li><a href="mailto:olux.moore@gmail.com" class="meta-mail">yourmail.@gmail.com</a>
+                                    </li>
+                                    <li><a href="tel:012345678" class="meta-phone">012 345 678 9101</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer><!-- /#footer -->
+        <!-- Bottom -->
+        <div id="bottom" class="clearfix has-spacer">
+            <div id="bottom-bar-inner" class="container">
+                <div class="bottom-bar-inner-wrap">
+                    <div class="bottom-bar-content-left link-style-4">
+                        <div id="copyright"><span class="text">Copyright © 2021 The Business Consulting HTML Template.
+                                Design by <a
+                                    href="https://themeforest.net/user/themesflat/portfolio">themesflat</a></span>
+                        </div>
+                    </div>
+                    <div class="bottom-bar-content-right">
+                        <div class="widget-links-bottom link-style-4">
+                            <ul>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="service-details.html">Privacy</a></li>
+                                <li><a href="service-details.html">Policy</a></li>
+                                <li><a href="service-details.html">Customer</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- /#bottom -->
+
+    </div>
+    <!-- /#page -->
+    </div>
+    <!-- /#wrapper -->
+
+    <a id="scroll-top"></a>
+
+    <!-- Javascript -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/plugin.js"></script>
+    <script src="assets/js/jquery-validate.js"></script>
+    <script src="assets/js/countto.js"></script>
+    <script src="assets/js/jquery.fancybox.js"></script>
+    <script src="assets/js/shortcodes.js"></script>
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="assets/js/swiper.js"></script>
+    <script src="assets/js/main.js"></script>
---
+++ header.php
@@ -0,0 +1,121 @@
+ <!-- Header Wrap -->
+ <div id="site-header-wrap">
+     <!-- /#site-logo -->
+     <!-- Header -->
+     <header id="site-header">
+         <div id="site-logo" class="clearfix">
+             <div id="site-logo-inner">
+                 <a href="index.html" rel="home" class="main-logo">
+                     <img src="assets/images/logo/logo-light.png" alt="consalti" width="183" height="48"
+                         data-retina="assets/images/logo/logo-light@3x.png" data-width="183" data-height="48">
+                 </a>
+             </div>
+         </div>
+         <div class="header-box">
+             <!-- Top Bar -->
+             <div id="top-bar">
+                 <div id="top-bar-inner">
+                     <div class="top-bar-content">
+                         <div class="top-bar-left">
+                             <span class="map section-14px-regular">Tungmahamek, Sathorn, Bangkok, Thailand
+                                 10120</span>
+                             <span class="mail section-14px-regular"><a
+                                     href="mailto:consalti.business@gmail.com">consalti.business@gmail.com</a></span>
+                         </div>
+                         <div class="top-bar-right">
+                             <span class="phone section-14px-regular"><a href="tel:012345678">+12 3 456
+                                     7890</a></span>
+                             <div class="socials-header">
+                                 <ul class="widget-socials link-style-3">
+                                     <li><a href="#" class="facebook"></a></li>
+                                     <li><a href="#" class="twitter"></a></li>
+                                     <li><a href="#" class="linked-in"></a></li>
+                                     <li><a href="#" class="instagram"></a></li>
+                                 </ul>
+                             </div>
+                         </div>
+                     </div>
+                     <!-- /.top-bar-content -->
+                 </div>
+             </div>
+             <!-- /#top-bar -->
+             <div id="site-header-inner">
+                 <div class="wrap-inner clearfix">
+                     <div class="mobile-button">
+                         <span></span>
+                     </div>
+                     <!-- /.mobile-button -->
+                     <nav id="main-nav" class="main-nav">
+                         <ul id="menu-primary-menu" class="menu">
+                             <li class="menu-item current-menu-item">
+                                 <a href="index.html">Home</a>
+                             </li>
+                             <li class="menu-item">
+                                 <a href="about.html">About Us </a>
+                             </li>
+                             <li class="menu-item menu-item-has-children">
+                                 <a href="service.html">Services</a>
+                                 <ul class="sub-menu">
+                                     <li class="menu-item"><a href="service.html">Services</a></li>
+                                     <li class="menu-item"><a href="service-details.html">Service Details</a>
+                                     </li>
+                                 </ul>
+                             </li>
+                             <li class="menu-item">
+                                 <a href="team.html">Team</a>
+                             </li>
+                             <li class="menu-item">
+                                 <a href="faq.html">Faq</a>
+                             </li>
+                             <li class="menu-item menu-item-has-children">
+                                 <a href="portfolio.html">Portfolio</a>
+                                 <ul class="sub-menu">
+                                     <li class="menu-item"><a href="portfolio.html">Portfolio</a></li>
+                                     <li class="menu-item"><a href="portfolio-details.html">Portfolio
+                                             Details</a>
+                                     </li>
+                                 </ul>
+                             </li>
+                             <li class="menu-item menu-item-has-children">
+                                 <a href="blog.html">Blog</a>
+                                 <ul class="sub-menu">
+                                     <li class="menu-item"><a href="blog.html">Blog</a></li>
+                                     <li class="menu-item"><a href="blog-details.html">Blog Single</a></li>
+                                 </ul>
+                             </li>
+                             <li class="menu-item">
+                                 <a href="contact.html">Contact</a>
+                             </li>
+                         </ul>
+                     </nav>
+                     <!-- /#main-nav -->
+                 </div>
+                 <!-- /.wrap-inner -->
+                 <div id="site-header-right">
+                     <div class="header-search flat-show-search">
+                         <a href="#" class="header-search-trigger show-search"></a>
+                         <div class="widget widget-search top-search">
+                             <form action="#" method="get" role="search" class="search-form">
+                                 <input type="search" id="s" class="search-field" placeholder="Search keywords" value=""
+                                     name="s" title="Search for" required="">
+                                 <button class="search search-submit" type="submit" title="Search"></button>
+                             </form>
+                         </div>
+                     </div>
+                     <div class="header-menu">
+                         <!-- <a href="#" class="header-menu-trigger"></a> -->
+                         <div class="mobile-nav-toggler">
+                             <span class="bar"></span><!-- /.bar -->
+                             <span class="bar"></span><!-- /.bar -->
+                             <span class="bar"></span><!-- /.bar -->
+                         </div>
+                     </div>
+                     <div class="button-header">
+                         <a href="contact.html">GET REQUEST</a>
+                     </div>
+                 </div>
+             </div>
+             <!-- /#site-header-inner -->
+         </div>
+     </header>
+     <!-- /#site-header -->
